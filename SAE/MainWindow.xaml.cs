b6641d95ed85335529c89c44cd4e89e534e1176a
Edgar Bedu-Neyrinck <Edgar.Bedu-Neyrinck@etu.univ-smb.fr>
2024-12-09 17:00:27 +0100

    Menu animé

--- SAE/MainWindow.xaml.cs
+++ SAE/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,6 +21,24 @@ namespace SAE
         {
             InitializeComponent();
 
+            // Démarre les animations
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Obtenir les dimensions réelles de l'image
+            var luneTransform = (RotateTransform)imgLune.RenderTransform;
+            luneTransform.CenterX = imgLune.ActualWidth / 2;
+            luneTransform.CenterY = imgLune.ActualHeight / 2;
+
+            // Lancer l'animation
+            var moonStoryboard = (Storyboard)FindResource("RotateMoonStoryboard");
+            moonStoryboard.Begin();
+
+            // Lance l'animation de montée et descente pour imgAstro
+            var astroStoryboard = (Storyboard)FindResource("MoveAstronautStoryboard");
+            astroStoryboard.Begin();
         }
 
         private void butJouer_Click(object sender, RoutedEventArgs e)
